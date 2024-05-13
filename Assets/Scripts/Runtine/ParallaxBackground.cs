73519a5e04d5f180116c0d09207807019add68d1
ttmVi <132742752+ttmVi@users.noreply.github.com>
2024-05-13 11:06:34 +0700

    Added Script for Parallax Background

---
+++ Assets/Scripts/Runtine/ParallaxBackground.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    private float length;
+    private Vector2 startPos;
+    [SerializeField] private GameObject cam;
+    [SerializeField] private float parallaxFactorX;
+    [SerializeField] private float parallaxFactorY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxFactorX));
+
+        float distanceX = cam.transform.position.x * parallaxFactorX;
+        float distanceY = cam.transform.position.y * parallaxFactorY;
+
+        transform.position = new Vector2(startPos.x + distanceX, startPos.y + distanceY);
+
+        if (temp > startPos.x + length)
+        {
+            startPos += new Vector2(length, 0f);
+        }
+    }
+}
