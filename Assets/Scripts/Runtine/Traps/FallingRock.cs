d8cb50bea6bfabed9058cc472625d834b388229c
ttmVi <s3979371@rmit.edu.vn>
2024-05-14 19:30:13 +0700

    Added 3 Traps in Dungeon
    - Spikes
    - Falling Rocks
    - Gas Geyser

---
+++ Assets/Scripts/Runtine/Traps/FallingRock.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingRock : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GetComponent<KnockbackTrigger>().enabled = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GetComponent<KnockbackTrigger>().enabled = true;
+        }
+    }
+}
