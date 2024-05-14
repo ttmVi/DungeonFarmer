5c4a2b76701540c2f24ce19d26db8d98b8ae183c
ttmVi <s3979371@rmit.edu.vn>
2024-05-14 23:48:59 +0700

    minor tweaks to traps

--- Assets/Scripts/Runtine/Traps/FallingRock.cs
+++ Assets/Scripts/Runtine/Traps/FallingRock.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class FallingRock : MonoBehaviour
 {
+    private void Update()
+    {
+        if (transform.position.y < -50)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
